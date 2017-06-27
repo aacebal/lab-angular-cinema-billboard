a4af8a7fbf48f6de5c8602ef6605add334ba261c
Adel Acebal <adelacebal@yahoo.com>
2017-06-26 21:13:01 -0400

    style done

--- starter-code/src/app/movie-information.service.ts
+++ starter-code/src/app/movie-information.service.ts
@@ -8,7 +8,7 @@ export class MovieInformationService {
   movies: Object[] = [{
     id: 1,
     title: "The Shawshank Redemption",
-    poster: "https://i.imgur.com/SuW2ZlC.jpg",
+    poster: "https://fanart.tv/fanart/movies/278/movieposter/the-shawshank-redemption-5223c8231b88e.jpg",
     synopsis: "In 1947, Andy Dufresne (Tim Robbins), a banker in Maine, is convicted of murdering his wife and her lover, a golf pro. Since the state of Maine has no death...",
     genres: [
       "Crime", "Drama"
@@ -26,7 +26,7 @@ export class MovieInformationService {
   }, {
     id: 2,
     title: "The Godfather",
-    poster: "https://i.imgur.com/Uzvny9I.jpg",
+    poster: "https://boredanddangerousblog.files.wordpress.com/2015/12/godfather-1.jpg",
     synopsis: "In late summer 1945, guests are gathered for the wedding reception of Don Vito Corleone's daughter Connie (Talia Shire) and Carlo Rizzi (Gianni Russo). Vito...",
     genres: [
       "Crime", "Drama"
@@ -44,7 +44,7 @@ export class MovieInformationService {
   }, {
     id: 3,
     title: "The Godfather Part II",
-    poster: "https://i.imgur.com/abJNkWI.jpg",
+    poster: "https://s3.amazonaws.com/nightjarprod/content/uploads/2015/06/the_godfather_part_2-al-pacino-poster.jpg",
     synopsis: "The Godfather Part II presents two parallel storylines. One involves Mafia chief Michael Corleone in 1958/1959 after the events of the first movie; the othe...",
     genres: [
       "Crime", "Drama"
@@ -62,7 +62,7 @@ export class MovieInformationService {
   }, {
     id: 4,
     title: "The Dark Knight",
-    poster: "https://i.imgur.com/3jLPB46.jpg",
+    poster: "https://static.comicvine.com/uploads/original/0/4115/885630-1new_joker_poster_for_the_dark_knight.jpg",
     synopsis: "The movie begins with a gang of men with clown masks breaking into the bank where the mob has a large portion of their money stashed. It begins with five cl...",
     genres: [
       "Action", "Crime", "Drama", "Thriller"
@@ -81,7 +81,7 @@ export class MovieInformationService {
   {
     id: 5,
     title: "Schindler's List",
-    poster: "https://i.imgur.com/IWZJOmu.jpg",
+    poster: "https://i.jeded.com/i/schindlers-list.22323.jpg",
     synopsis: "The relocation of Polish Jews from surrounding areas to Krakow begins in late 1939, shortly after the outbreak of World War II, when the German Army defeats...",
     genres: [
       "Biography", "Drama", "History"
